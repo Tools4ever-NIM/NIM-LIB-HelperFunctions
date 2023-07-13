97ec4bdcf604be95afa7d75d3a9e752a8c49f4e1
sblankenshipt4e <134305269+sblankenshipt4e@users.noreply.github.com>
2023-07-13 13:56:42 -0700

    Update Lib.js
    Changes methods to static

--- Lib.js
+++ Lib.js
@@ -1,9 +1,7 @@
 return class {
 
-  /*
-    Strip down text to be compared with other text
-  */
-  cleanName(name) {
+  /* Strip down text to be compared with other text */
+  static cleanName(name) {
     let result = name;
     result = result.toLowerCase(); // Set to Lowercase
     result = result.replace(/\W/g, ''); // Remove Special Characters
@@ -11,32 +9,23 @@ return class {
     return result;
   }
 
-  /*
-    Title Casing for a string 
-    (e.g. "MICHAEL JOHNSON-SMITH" to "Michael Johnson-Smith"
-  */
-  titleCase(str) {
+  /* Title Casing for a string (e.g. "MICHAEL JOHNSON-SMITH" to "Michael Johnson-Smith" */
+  static titleCase(str) {
     return str.toLowerCase().replace(/(?:^|[\s-/]|[\s'])\w/g, function (match) {
       return match.toUpperCase();
     });
   }
 
-  /*
-    Removes the User CN from the CanoncalName
-  */
-  cleanCanonicalName(canonicalName) {
+  /* Removes the User CN from the CanoncalName */
+  static cleanCanonicalName(canonicalName) {
     result = canonicalName.split('/');
     result.shift();
     result.pop();
     return `/${result.join('/')}`;
   }
 
-  /**
-   * Returns the text ordinal of the val
-   * 
-   * @param val The number to find ordinal for.
-  */
-  getOrdinalSuffix(val) {
+  /* Returns the text ordinal of the val */
+  static getOrdinalSuffix(val) {
     var x = Number(val) % 10,
       y = Number(val) % 100;
     var suffix = "th";
@@ -51,21 +40,13 @@ return class {
     return suffix;
   }
 
-  /*
-    Replace Diacritic Marks and Curly Quotes
-  */
-  function removeDiacriticMarks(str) {
+  /* Replace Diacritic Marks and Curly Quotes */
+  static function removeDiacriticMarks(str) {
     return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[\u2018\u2019]/g, "'");
   }
 
-  /* 
-    Use the following solution to use the generated value as a string
-    generateRandomInteger(1000, 9999).toString();
-    
-    Use the following Make sure random integer is 2 characters long, if not, prefix with '0'
-    ('000000' + generateRandomInteger(1000, 9999).toString()).slice(-6);
-  */
-  function generateRandomInteger(min, max) {
+  /* Generates a random number in the given range */
+  static function generateRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + Iteration)) + min;
   }
 }
